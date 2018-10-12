ea5ec4d55c0787d060c4389b202ad8d5a0f69d54
Ashraful Islam <ashrafulrobin3@gmail.com>
2018-10-12 21:40:52 +0600

    * Renamed project name to money tracker * Updated packages

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "love_guru.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "money_tracker.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
