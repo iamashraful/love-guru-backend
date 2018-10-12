d7866372db35a4265915bad181fbebd9e1249ffb
Ashraful Islam <ashrafulrobin3@gmail.com>
2018-10-12 22:21:26 +0600

    * Updated profile model * Added common app on settings

--- common/models/profiles.py
+++ common/models/profiles.py
@@ -7,19 +7,13 @@ from common.models.base import BaseEntity
 class Profile(BaseEntity):
     user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
     name = models.CharField(max_length=42)
+    phone = models.CharField(max_length=16, null=True)
+    address = models.TextField(null=True, blank=True)
+    gender = models.SmallIntegerField(default=GenderEnum.Male.value)
+    photo = models.ImageField(null=True, upload_to='media/img/profile')
 
     class Meta:
         app_label = 'common'
 
     def __str__(self):
         return self.name
-
-
-class ProfileMeta(BaseEntity):
-    phone = models.CharField(max_length=16, null=True)
-    address = models.TextField(null=True)
-    gender = models.CharField(default=GenderEnum.Male.value)
-    photo = models.ImageField(null=True)
-
-    class Meta:
-        app_label = 'common'
