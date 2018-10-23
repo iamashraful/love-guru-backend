4aee6b55079d391d24934369283344fbc5d68195
Ashraful <ashrafulrobin3@gmail.com>
2018-10-23 17:39:13 +0600

    Added models for transaction

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -1,9 +1,10 @@
 from .base import BaseEntity
 from .profiles import Profile
+from .transactions import Wallet, Transaction, Category
 
 __author__ = 'Ashraful'
 
 
 __all__ = [
-    'BaseEntity', 'Profile'
+    'BaseEntity', 'Profile', Wallet, Transaction, Category
 ]
---
+++ api/models/transactions.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+from api.models import BaseEntity
+
+__author__ = 'Ashraful'
+
+
+class Category(BaseEntity):
+    name = models.CharField(max_length=32)
+
+    class Meta:
+        app_label = 'api'
+
+
+class Wallet(BaseEntity):
+    name = models.CharField(max_length=64)
+    balance = models.FloatField(default=0)
+    initial_balance = models.FloatField(default=0, editable=False)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        app_label = 'api'
+
+
+class Transaction(BaseEntity):
+    name = models.CharField(max_length=64)
+    note = models.TextField(null=True, blank=True)
+    category = models.ForeignKey('api.Category', on_delete=models.SET_NULL, null=True)
+    wallet = models.ForeignKey('api.Wallet', on_delete=models.SET_NULL, null=True)
+    transaction_time = models.BigIntegerField(default=None, null=True)
+
+    class Meta:
+        app_label = 'api'
