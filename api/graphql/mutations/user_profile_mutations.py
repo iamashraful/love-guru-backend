0543462fcc017e0e9fa97979afe29729653ed2ea
Ashraful Islam <ashrafulrobin3@gmail.com>
2019-06-22 20:04:24 +0600

    Updated create user profile mutation to fit my personal requirement

--- api/graphql/mutations/user_profile_mutations.py
+++ api/graphql/mutations/user_profile_mutations.py
@@ -1,4 +1,5 @@
 import graphene
+from django.contrib.auth.models import User
 
 from api.graphql.schema.user_profile_schema import ProfileInput
 from api.graphql.types.user_profile_type import ProfileType, UserType
@@ -10,12 +11,20 @@ class CreateUserProfile(graphene.Mutation):
         input = ProfileInput(required=True)
 
     success = graphene.Boolean()
-    # user = graphene.Field(UserType)
     profile = graphene.Field(ProfileType)
 
     @staticmethod
     def mutate(root, info, input=None):
         success = True
-        _instance = Profile(name=input.name, address=input.address, phone=input.phone, gender=input.gender)
+        _user = input.user
+        _password = _user.get('password', '')
+        _confirm_password = _user.get('confirm_password', '')
+        if _password == _confirm_password:
+            _u = User(username=_user.get('username'))
+            _u.save()
+            _u.set_password(_password)
+        else:
+            raise Exception('password and confirm password should match')
+        _instance = Profile(user=_u, name=input.name, address=input.address, phone=input.phone, gender=input.gender)
         _instance.save()
-        return CreateUserProfile(status=success, profile=_instance)
+        return CreateUserProfile(success=success, profile=_instance)
--- api/graphql/schema/user_profile_schema.py
+++ api/graphql/schema/user_profile_schema.py
@@ -3,15 +3,12 @@ from graphene import InputObjectType
 
 
 class UserInput(InputObjectType):
-    id = graphene.ID()
     username = graphene.String()
-    first_name = graphene.String()
-    last_name = graphene.String()
-    is_active = graphene.Boolean()
+    password = graphene.String()
+    confirm_password = graphene.String()
 
 
 class ProfileInput(InputObjectType):
-    id = graphene.ID()
     name = graphene.String()
     address = graphene.String()
     phone = graphene.String()
@@ -19,3 +16,4 @@ class ProfileInput(InputObjectType):
     created_at = graphene.DateTime()
     updated_at = graphene.DateTime()
     is_active = graphene.Boolean()
+    user = graphene.Field(UserInput)
